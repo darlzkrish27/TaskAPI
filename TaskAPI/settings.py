68f3a8d691b5c7cbce95490a399f9881c2bc8eb8
darlzkrish27 <darlzkrish27@gmail.com>
2017-01-23 20:56:51 +0530

    first commit

--- TaskAPI/settings.py
+++ TaskAPI/settings.py
@@ -105,10 +105,10 @@ STATIC_ROOT=os.path.join(BASE_DIR, "static")
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
-    '/static/',
-)
+#STATICFILES_DIRS = (
+ #   os.path.join(BASE_DIR, "static"),
+  #  '/static/',
+#)
 
 
 
